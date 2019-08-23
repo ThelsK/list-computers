eb7db84e64b6eab30efd36984817c3c328c85559
ThelsK <github@thels.nl>
2019-08-23 19:29:06 +0200

    Added select list and populated it

--- src/App.js
+++ src/App.js
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    const data = [
+      {
+        name: "Ivel Z3",
+        manufacturer: "Ivasim",
+        year: 1969,
+        origin: "Croatia"
+      },
+      {
+        name: "Bally Astrocade",
+        manufacturer: "Bally Consumer Products",
+        year: 1977,
+        origin: "USA"
+      },
+      {
+        name: "Sord M200 Smart Home Computer",
+        manufacturer: "Sord Computer Corporation",
+        year: 1971,
+        origin: "Japan"
+      },
+      {
+        name: "Commodore 64",
+        manufacturer: "Commodore",
+        year: 1982,
+        origin: "USA"
+      },
+      {
+        name: "MSX",
+        manufacturer: "ASCII Corporation",
+        year: 1983,
+        origin: "Japan"
+      },
+    ]
+    return (
+      <div className="App-header">
+        <select>
+          <option value="">-- pick a model --</option>
+          {data.map(dataEntry =>
+            <option value={dataEntry.name}>{dataEntry.name} ({dataEntry.year})</option>
+          )}
+        </select>
+      </div>
+    )
+  }
 }
 
-export default App;
+export default App
